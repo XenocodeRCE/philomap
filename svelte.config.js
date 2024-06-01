f44b5cf51ddc5407bdda7cca4bc38151a1a20835
Mr Roux <XenocodeRCE@users.noreply.github.com>
2024-06-01 22:32:26 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,30 @@
 /* import adapter from '@sveltejs/adapter-auto'; */
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        appDir: 'app', // Required as the default is _app
-        adapter: adapter()
-    },
-    preprocess: vitePreprocess()
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === "production" ? "/myMarkmap" : ""
+			},
+	}
 };
-export default config;
+
+/* export default config;  */
+export default {
+	kit: {
+	  adapter: adapter({
+		// default options are shown. On some platforms
+		// these options are set automatically — see below
+		pages: 'build',
+		assets: 'build',
+		fallback: null,
+		precompress: false,
+		strict: true
+	  }),
+	  prerender: {
+		handleMissingId: 'ignore'
+	}
+	}
+  };
